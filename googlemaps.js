8751535faa7815c01c471019ce89002368eb77db
aangeles02 <54568738+aangeles02@users.noreply.github.com>
2019-11-14 23:37:01 -0600

    added 3 additonal features

--- googlemaps.js
+++ googlemaps.js
@@ -1,5 +1,23 @@
 function initMap() {
-  var uluru = {lat: -25.344, lng: 131.036};
+  var uluru = {lat:41.8781, lng: -87.6298};
   var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: uluru}
+			
+			
+			);
   var marker = new google.maps.Marker({position: uluru, map: map});
+	var school = {
+		lat: 41.9658,
+		lng: -87.7142
+	};
+	var marker = new google.maps.Marker( {
+	position: school,
+	map: map,
+	title: 'Theodore Roosevelt High School'
+});
+  var bikeLayer = new google.maps.BicyclingLayer();
+  bikeLayer.setMap(map);
+	  var transitLayer = new google.maps.TransitLayer();
+  transitLayer.setMap(map);
+	
 }
