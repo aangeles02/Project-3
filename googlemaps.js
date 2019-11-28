96cd71f55da65f72b48112c309c6d77a7708ea03
aangeles02 <54568738+aangeles02@users.noreply.github.com>
2019-11-28 16:18:38 -0600

    added js to slide show

--- googlemaps.js
+++ googlemaps.js
@@ -21,3 +21,17 @@ function initMap() {
   transitLayer.setMap(map);
 	
 }
+var slideIndex = 0;
+showSlides();
+function showSlides() {
+	var i;
+	var slides = document.getElementByClassName("mySlides");
+	for (i=0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+}
+ slideInedx++;
+ if(slideIndex > slides.length) {slideIndex =1}
+ slides[slideIndex-1].style.display ="block";
+ setTimeout(showSlides, 3500);
+}
+
